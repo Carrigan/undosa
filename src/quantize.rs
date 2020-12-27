1c787d2021ae9c75122ecd591b57ec115533df07
Brian Carrigan <brian@bcarrigan.com>
2020-12-27 08:24:43 -0500

    Improve quantize API

--- src/quantize.rs
+++ src/quantize.rs
@@ -5,7 +5,13 @@ pub fn samples_in_beat(tempo: u8) -> u32 {
     30 * 48000 / (tempo as u32)
 }
 
-pub fn quantize<A: WaveGenerator>(generator: A, tempo: u8, duration: u8, cutoff: u8) -> Take<A> {
-    let take_amount = samples_in_beat(tempo) * (duration as u32) * (cutoff as u32 + 1) / 256;
-    generator.take(take_amount as usize)
+pub trait Quantizable<T>: WaveGenerator {
+    fn quantize(self, tempo: u8, duration: u8, cutoff: u8) -> Take<T>;
+}
+
+impl <T> Quantizable<T> for T where T: WaveGenerator {
+    fn quantize(self, tempo: u8, duration: u8, cutoff: u8) -> Take<T> {
+        let take_amount = samples_in_beat(tempo) * (duration as u32) * (cutoff as u32 + 1) / 256;
+        self.take(take_amount as usize)
+    }
 }
